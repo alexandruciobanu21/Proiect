b2d784436559dc297c850b7bf418b87b3d1b7861
alexandruciobanu21 <35220279+alexandruciobanu21@users.noreply.github.com>
2021-09-08 01:34:52 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-class Kernel:
-    def __init__(self, context, queue, memF, work_group, program):
-        self.context = context
-        self.queue = queue
-        self.memF = memF
-        self.work_group = work_group
-        self.program = program
